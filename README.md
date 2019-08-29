38ebe097786008f9d52b983c35085f98163ff842
Michal Fratczak <michal@cgarea.com>
2019-08-29 18:05:31 +0200

    some readme update

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 Example usage and config of tilemaker.
 
+First, you need to download OSM data in pbf format.
+Get it from here:
+    http://download.geofabrik.de/europe/poland.html
+    http://download.geofabrik.de/europe/poland-latest.osm.pbf
+
+Tilemaker is executed from update.sh script.
+Take a look inside - the process is divided into layers, because tilemaker can be mem hungry, besides, for me, it's easier to work that way ...
+
+Example usage:
+
+```./update.sh ./poland-latest.osm.pbf ./poland_tiles```
+
+You're now ready to serve your tiles:
+    
+    - with apache:
+        sudo mkdir /var/www/html/osm
+        sudo chmod a+rwx /var/www/html/osm
+        cp ./static/index.html /var/www/html/osm/
+        sudo ln -s ./poland_tiles /var/www/html/osm/poland_tiles
+
+    or
+
+    - with python bottle (server.py): ./server.py
+
+
 todo:
 - fix server to actually serve some html (and not only tiles)
 - fix html with comments and paths to OSM directiories
 - add font download location
-
-Download PBF:
-http://download.geofabrik.de/europe/poland.html
-http://download.geofabrik.de/europe/poland-latest.osm.pbf
--- update.sh
+++ update.sh
@@ -4,10 +4,10 @@
 # $2 is output dir
 
 mkdir $2
-# /usr/bin/time -v ./tilemaker --config ./hab_aeroway.json 	--process ./hab_aeroway.lua 	--input $1 --output $2/aeroway
-# /usr/bin/time -v ./tilemaker --config ./hab_buildings.json 	--process ./hab_buildings.lua 	--input $1 --output $2/buildings
-# /usr/bin/time -v ./tilemaker --config ./hab_landuse.json 	--process ./hab_landuse.lua 	--input $1 --output $2/landuse
-/usr/bin/time -v ./tilemaker --config ./hab_roads.json 		--process ./hab_roads.lua 		--input $1 --output $2/roads
-# /usr/bin/time -v ./tilemaker --config ./hab_water.json 		--process ./hab_water.lua 		--input $1 --output $2/water
-# /usr/bin/time -v ./tilemaker --config ./hab_labels.json 	--process ./hab_labels.lua 		--input $1 --output $2/labels
-# /usr/bin/time -v ./tilemaker --config ./hab_admin.json 		--process ./hab_admin.lua 		--input $1 --output $2/admin
+# /usr/bin/time -v ./tilemaker --combine 0 --config ./hab_aeroway.json 	--process ./hab_aeroway.lua 	--input $1 --output $2/aeroway
+# /usr/bin/time -v ./tilemaker --combine 0 --config ./hab_buildings.json 	--process ./hab_buildings.lua 	--input $1 --output $2/buildings
+# /usr/bin/time -v ./tilemaker --combine 0 --config ./hab_landuse.json 	--process ./hab_landuse.lua 	--input $1 --output $2/landuse
+/usr/bin/time -v ./tilemaker --combine 0 --config ./hab_roads.json 		--process ./hab_roads.lua 		--input $1 --output $2/roads
+# /usr/bin/time -v ./tilemaker --combine 0 --config ./hab_water.json 		--process ./hab_water.lua 		--input $1 --output $2/water
+# /usr/bin/time -v ./tilemaker --combine 0 --config ./hab_labels.json 	--process ./hab_labels.lua 		--input $1 --output $2/labels
+# /usr/bin/time -v ./tilemaker --combine 0 --config ./hab_admin.json 		--process ./hab_admin.lua 		--input $1 --output $2/admin
